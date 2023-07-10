deeb46d1b6e170061280e4d9db640b411ca101bd
matvey2023 <136736902+matvey2023@users.noreply.github.com>
2023-07-10 17:32:42 +0300

    Add files via upload

---
+++ 4 модуль/урок1 модуль4.py
@@ -0,0 +1,18 @@
+def str_counter(s):
+    for sym in set(s):
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter +=1
+        print(sym, "-", counter)
+
+# str_counter('aabbcd')
+
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym]=syms_counter.get(sym, 0)+1
+
+    for sym, count in syms_counter.items():
+        print(sym, " - ", count)
+strcounter('qwerttttty')
